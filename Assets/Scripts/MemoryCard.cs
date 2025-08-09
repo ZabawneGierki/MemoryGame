a3966440f34ebcd64818bc3d69970b01265f652d
krzysztofskorek <57440106+krzysztofskorek@users.noreply.github.com>
2025-08-09 18:41:45 +0200

    created Memory Game Manager Script

--- Assets/Scripts/MemoryCard.cs
+++ Assets/Scripts/MemoryCard.cs
@@ -1,18 +1,51 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MemoryCard : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int id; // unique ID for matching
+    public Sprite frontImage;
+    public Sprite backImage;
+
+    private Image imageComponent;
+    private Button button;
+    private bool isRevealed = false;
+    private MemoryGameManager gameManager;
+
+    public void Init(MemoryGameManager manager, int id, Sprite front, Sprite back)
     {
-        
+        gameManager = manager;
+        this.id = id;
+        frontImage = front;
+        backImage = back;
+
+        imageComponent = GetComponent<Image>();
+        button = GetComponent<Button>();
+
+        Hide();
+        button.onClick.AddListener(OnClick);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Show()
     {
-        
+        isRevealed = true;
+        imageComponent.sprite = frontImage;
+    }
+
+    public void Hide()
+    {
+        isRevealed = false;
+        imageComponent.sprite = backImage;
+    }
+
+    private void OnClick()
+    {
+        if (!isRevealed)
+            gameManager.CardRevealed(this);
+    }
+
+    public void Remove()
+    {
+        gameObject.SetActive(false);
     }
 }
---
+++ Assets/Scripts/MemoryGameManager.cs
@@ -0,0 +1,92 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class MemoryGameManager : MonoBehaviour
+{
+    public Sprite[] cardImages; // pair images
+    public Sprite cardBack;
+    public GameObject cardPrefab;
+    public Transform gridParent; // where cards go
+
+    private MemoryCard firstRevealed;
+    private MemoryCard secondRevealed;
+    private int totalPairs;
+    private int pairsFound;
+
+    void Start()
+    {
+        SetupBoard();
+    }
+
+    void SetupBoard()
+    {
+        List<int> ids = new List<int>();
+
+        // create pairs (2 of each)
+        for (int i = 0; i < cardImages.Length; i++)
+        {
+            ids.Add(i);
+            ids.Add(i);
+        }
+
+        // shuffle
+        for (int i = 0; i < ids.Count; i++)
+        {
+            int rand = Random.Range(i, ids.Count);
+            (ids[i], ids[rand]) = (ids[rand], ids[i]);
+        }
+
+        totalPairs = cardImages.Length;
+        pairsFound = 0;
+
+        // spawn cards
+        foreach (var id in ids)
+        {
+            GameObject cardObj = Instantiate(cardPrefab, gridParent);
+            MemoryCard card = cardObj.GetComponent<MemoryCard>();
+            card.Init(this, id, cardImages[id], cardBack);
+        }
+    }
+
+    public void CardRevealed(MemoryCard card)
+    {
+        if (firstRevealed == null)
+        {
+            firstRevealed = card;
+            card.Show();
+        }
+        else if (secondRevealed == null && card != firstRevealed)
+        {
+            secondRevealed = card;
+            card.Show();
+
+            StartCoroutine(CheckMatch());
+        }
+    }
+
+    private System.Collections.IEnumerator CheckMatch()
+    {
+        yield return new WaitForSeconds(0.5f);
+
+        if (firstRevealed.id == secondRevealed.id)
+        {
+            firstRevealed.Remove();
+            secondRevealed.Remove();
+            pairsFound++;
+
+            if (pairsFound >= totalPairs)
+            {
+                Debug.Log("You win! Image revealed.");
+            }
+        }
+        else
+        {
+            firstRevealed.Hide();
+            secondRevealed.Hide();
+        }
+
+        firstRevealed = null;
+        secondRevealed = null;
+    }
+}
